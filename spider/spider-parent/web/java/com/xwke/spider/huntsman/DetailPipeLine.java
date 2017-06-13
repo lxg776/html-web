5f6b1c874a2fb6c78d9ccbda3bd8d1edeaae8fd7
lxg776 <262621979@qq.com>
2017-06-14 00:07:00 +0800

    0613444
    0613444

---
+++ spider/spider-parent/web/java/com/xwke/spider/huntsman/DetailPipeLine.java
@@ -0,0 +1,97 @@
+package com.xwke.spider.huntsman;
+
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.fastjson.JSONArray;
+import com.xwke.spider.huntsman.configuration.NewsConfiguration;
+import com.xwke.spider.huntsman.util.CommonUtil;
+
+import com.xwke.spider.modle.NewsModle;
+import com.xwke.spider.modle.dao.NewsDao;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.pipeline.FilePipeline;
+import us.codecraft.webmagic.selector.Html;
+import us.codecraft.xsoup.Xsoup;
+
+@Component("detailPipeLine")
+public class DetailPipeLine extends FilePipeline {
+
+	public static String PARM_HTML = "dhtml";
+	public static String CONFIG = "newsConfig";
+	
+	@Resource
+	ThreadPoolTaskExecutor taskExecutor;
+	
+	@Resource
+	NewsDao newDao;
+	
+
+	@Override
+	public void process(ResultItems resultItems, Task task) {
+		
+		
+		NewsModle newsModle=new NewsModle();
+		Page page =resultItems.get(PARM_HTML);
+		NewsConfiguration config = resultItems.get(CONFIG);
+		if(page==null){
+			return;
+		}
+		
+		
+		Html html = page.getHtml();
+		
+		
+		String sourceUrl = page.getUrl().get();
+		if (null != html) {
+			String content = html.$(".container .show").get();
+			Document document = Jsoup.parse(content);
+			String title = Xsoup.compile("//h1/text()").evaluate(document).get().toString();
+			//String date = Xsoup.compile("//div[@class='info']/span").evaluate(document).get().toString();
+			
+			String text=Xsoup.compile("//div[@class='info']/span").evaluate(document).get().toString();
+			text=text.replaceAll("&nbsp;", "");
+			String date=text.substring(text.indexOf("<span>")+"<span>".length(), text.indexOf("来源："));
+			String source=text.substring(text.indexOf("来源：")+"来源：".length(), text.indexOf("评论："));
+			System.out.println(date);
+			System.out.println(source);
+			
+			
+			String htmlContent = Xsoup.compile("//div[@class='main']").evaluate(document).getElements().html().toString();
+			System.out.println(htmlContent);
+			
+			List<String> imgUrls = html.$(".container .show img")
+					.regex("<img(?:\\s+.+?)*?\\s+src=\"([^\"]*?)\".+>").all();
+			
+			String imagesString=JSONArray.toJSONString(imgUrls);
+			
+		
+			
+
+			newsModle.setTitle(title);
+			newsModle.setPubTime(date);
+			//newsModle.setGraspingTime(MyDataUtil.getNowDate());
+			newsModle.setSourceUrl(sourceUrl);
+			newsModle.setSource(source);
+			newsModle.setImagesJsonStr(imagesString);
+			newsModle.setContent(htmlContent);
+			//下载网上图片
+			CommonUtil.handleImagesByContent(newsModle, imgUrls, config,taskExecutor);
+			newDao.add(newsModle);
+			
+
+		}
+
+	}
+
+}
---
+++ spider/spider-parent/web/java/com/xwke/spider/huntsman/configuration/BaseConfiguration.java
@@ -0,0 +1,42 @@
+package com.xwke.spider.huntsman.configuration;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import us.codecraft.webmagic.Site;
+
+public class BaseConfiguration extends AbstractConfiguration {
+
+	protected Site site;
+	protected String baseDir;
+	
+	
+	
+
+	public BaseConfiguration(String path) {
+		super(path);
+	}
+
+	public Site getSite() {
+		return site;
+	}
+
+	public String getBaseDir() {
+		return baseDir;
+	}
+	
+	
+
+	public void setBaseDir(String baseDir) {
+		this.baseDir = baseDir;
+	}
+
+	@Override
+	protected void resolve() {
+		// TODO Auto-generated method stub
+		JSONObject jsonObject = JSON.parseObject(config);
+		site = JSON.parseObject(jsonObject.getString("site"), Site.class);
+		setBaseDir(jsonObject.getString("base_dir"));
+	}
+
+}
